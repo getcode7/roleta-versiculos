0e155c028d9895063da0e04c254fd4777a53925b
Ecleber Monteiro <120273455+getcode7@users.noreply.github.com>
2025-10-03 16:55:15 +0100

    Update server.js
    Corrigindo urls

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const db = new sqlite3.Database("./comentarios.db");
 
 // Configurar CORS para permitir apenas o domínio do frontend (substitua pela sua URL real)
 app.use(cors({
-  origin: "https://seu-frontend-render.onrender.com",
+  origin: "https://roleta-versiculos-4.onrender.com",
   optionsSuccessStatus: 200
 }));
 
